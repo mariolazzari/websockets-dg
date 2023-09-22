6d7770b43d38c4d75008e5734b2211494f9dc1f5
Mario Lazzari <mario.lazzari@gmail.com>
2023-09-22 23:50:52 +0200

    done lesson 2

--- ws-2/app/app.js
+++ ws-2/app/app.js
@@ -1,10 +1,10 @@
-const socket = new WebSocket("ws://localhost:3000");
+const socket = io("ws://localhost:3500");
 
 const sendMessage = e => {
   e.preventDefault();
   const input = document.querySelector("input");
   if (input.value) {
-    socket.emit("message", input.value);
+    socket.send(input.value);
     input.value = "";
   }
   input.focus();
@@ -12,7 +12,7 @@ const sendMessage = e => {
 
 document.querySelector("form").addEventListener("submit", sendMessage);
 
-socket.on("message", ({ data }) => {
+socket.on("message", data => {
   const li = document.createElement("li");
   li.textContent = data;
   document.querySelector("ul").appendChild(li);

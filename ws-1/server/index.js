e0ed28031de008b906aff83b689731ee3031e1f0
Mario Lazzari <mario.lazzari@gmail.com>
2023-09-22 22:00:32 +0200

    done lesson 1

---
+++ ws-1/server/index.js
@@ -0,0 +1,13 @@
+const ws = require("ws");
+
+const server = new ws.Server({
+  port: 3000,
+});
+
+server.on("connection", socket => {
+  socket.on("message", message => {
+    const buf = Buffer.from(message);
+    console.log(buf);
+    socket.send(buf.toString());
+  });
+});
